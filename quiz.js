2bf19a8c65573ecf11780e226f0e6ff04fd5a30e
Phantom <ramonadedotun550@gmail.com>
2025-06-14 15:27:11 +0100

    Simple quiz with predifined answer

---
+++ quiz.js
@@ -0,0 +1,13 @@
+function checkAnswer() {
+    let correctAnswer = "4";
+    const userAnswer = document.querySelector('input[name="quiz"]:checked')?.value; 
+    const feedback = document.getElementById("feedback");
+    if (userAnswer === correctAnswer) {
+        feedback.textContent = "Correct!, Well done.";
+    }else{
+        feedback.textContent = "That's incorrect. Try again!";
+    }
+    feedback.style.display = "block";
+}
+const submitAnswer = document.getElementById("submit-answer");
+submitAnswer.addEventListener("click",  checkAnswer);
